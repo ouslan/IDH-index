88b465c9cb67e482810bbaa1522b92d749927487
Alejandro Ouslan <unclearcoder@hotmail.com>
2023-11-18 21:06:31 -0400

    change data retrival checkpoint

--- src/data/IDH.py
+++ src/data/IDH.py
@@ -11,106 +11,87 @@ import os
 class IndexIDH:
 
     def health_index(self, year):
-        if os.path.exists('data/processed/health_index.csv'):
-            rt_health = pd.read_csv('data/processed/health_index.csv')
-            rt_health = rt_health.loc[rt_health['Year'] == year].iat[0, 1]
-            return rt_health
-        else:
-            # get Life expectancy at birth, total (years) for Puerto Rico
-            pr_health = pd.DataFrame(wb.get_series('SP.DYN.LE00.IN', country='PR', simplify_index=True))
-            pr_health.reset_index(inplace=True)
-            pr_health.rename(columns={'SP.DYN.LE00.IN': 'index'}, inplace=True)
-            pr_health['index'] = pr_health['index'].apply(lambda x: (x-20)/(85-20))
-            pr_health['index'] = pr_health['index'].astype(float)
-            pr_health['Year'] = pr_health['Year'].astype(int)
-            # save in csv
-            pr_health.to_csv('data/processed/health_index.csv', index=False)
-            # return health index for year 
-            rt_health = pr_health.loc[pr_health['Year'] == year].iat[0, 1]
-            # return only the health index for the year
-            return rt_health
+        # get Life expectancy at birth, total (years) for Puerto Rico
+        pr_health = pd.DataFrame(wb.get_series('SP.DYN.LE00.IN', country='PR', simplify_index=True))
+        pr_health.reset_index(inplace=True)
+        pr_health.rename(columns={'SP.DYN.LE00.IN': 'index'}, inplace=True)
+        pr_health['index'] = pr_health['index'].apply(lambda x: (x-20)/(85-20))
+        pr_health['index'] = pr_health['index'].astype(float)
+        pr_health['Year'] = pr_health['Year'].astype(int)
+        # save in csv
+        pr_health.to_csv('data/processed/health_index.csv', index=False)
 
     def income_index(self, year):
-        if os.path.exists('Data/income_index.csv'):
-            inc_df = pd.read_csv('Data/income_index.csv')
-            inc_df = inc_df.loc[inc_df['Year'] == year].iat[0, 1]
-            return inc_df
-        else:
-            # get atlas df from WB
-            atlas_df = pd.DataFrame(wb.get_series('NY.GNP.PCAP.CD', country='PR', simplify_index=True))
-            atlas_df.reset_index(inplace=True)
-            atlas_df.rename(columns={'NY.GNP.PCAP.CD': 'atlas'}, inplace=True)
-            atlas_df['atlas'] = atlas_df['atlas'].astype(float)
+        # get atlas df from WB
+        atlas_df = pd.DataFrame(wb.get_series('NY.GNP.PCAP.CD', country='PR', simplify_index=True))
+        atlas_df.reset_index(inplace=True)
+        atlas_df.rename(columns={'NY.GNP.PCAP.CD': 'atlas'}, inplace=True)
+        atlas_df['atlas'] = atlas_df['atlas'].astype(float)
 
-            # get gni constant df from WB
-            gni_df = pd.DataFrame(wb.get_series('NY.GNP.PCAP.PP.KD', country='PR', simplify_index=True))
-            gni_df.reset_index(inplace=True)
-            gni_df.rename(columns={'NY.GNP.PCAP.PP.KD': 'gni'}, inplace=True)
-            gni_df['gni'] = gni_df['gni'].astype(float)
+        # get gni constant df from WB
+        gni_df = pd.DataFrame(wb.get_series('NY.GNP.PCAP.PP.KD', country='PR', simplify_index=True))
+        gni_df.reset_index(inplace=True)
+        gni_df.rename(columns={'NY.GNP.PCAP.PP.KD': 'gni'}, inplace=True)
+        gni_df['gni'] = gni_df['gni'].astype(float)
 
-            # merge the two dataframes
-            inc_df = atlas_df.merge(gni_df, on='Year')
-            inc_df['income_ratio'] = inc_df['gni'] / inc_df['atlas']
-            inc_df['income_ratio'] = inc_df['income_ratio'].astype(float)
-            inc_df['Year'] = inc_df['Year'].astype(int)
+        # merge the two dataframes
+        inc_df = atlas_df.merge(gni_df, on='Year')
+        inc_df['income_ratio'] = inc_df['gni'] / inc_df['atlas']
+        inc_df['income_ratio'] = inc_df['income_ratio'].astype(float)
+        inc_df['Year'] = inc_df['Year'].astype(int)
 
-            # merge the income index with the pnb.csv file
-            pnb = pd.read_csv('pnb.csv')
-            merge_df = inc_df.merge(pnb, on='Year', how='left')
-            merge_df = merge_df.dropna()
-            merge_df.reset_index(inplace=True)
-            # drob the index column
-            merge_df.drop(['index'], axis=1, inplace=True)
-            merge_df['index_temp'] = merge_df['income_ratio'] * merge_df['pnb']
-            merge_df['index'] = (np.log(merge_df['index_temp']) - np.log(100)) / (np.log(70000)-np.log(100))
-            #remove temp data
-            merge_df = merge_df[['Year', 'index']]
-            # save the data
-            merge_df.to_csv('Data/income_index.csv', index=False)
-            # return the index value for that year
-            return merge_df.loc[merge_df['Year'] == year, 'index'].values[0]
+        # merge the income index with the pnb.csv file
+        pnb = pd.read_csv('pnb.csv')
+        merge_df = inc_df.merge(pnb, on='Year', how='left')
+        merge_df = merge_df.dropna()
+        merge_df.reset_index(inplace=True)
+        # drob the index column
+        merge_df.drop(['index'], axis=1, inplace=True)
+        merge_df['index_temp'] = merge_df['income_ratio'] * merge_df['pnb']
+        merge_df['index'] = (np.log(merge_df['index_temp']) - np.log(100)) / (np.log(70000)-np.log(100))
+        #remove temp data
+        merge_df = merge_df[['Year', 'index']]
+        # save the data
+        merge_df.to_csv('Data/income_index.csv', index=False)
+        # return the index value for that year
+        return merge_df.loc[merge_df['Year'] == year, 'index'].values[0]
     
-    def edu_index(self, year):
-        # check if edu index is already calculated
-        if os.path.exists('data/processed/edu_index.csv'):
-            data = pd.read_csv('data/processed/edu_index.csv')
-            # return the index value for that year
-            try:
-                return data[data['Year'] == year]['index'].values[0]
-            except (IndexError):
-                return np.nan
-        else:
-            try: 
-                data = f'data/processed/data_{year}_raw.csv'
-                self.df = pd.read_csv(data, low_memory=False)
-            except(FileNotFoundError):
-                print('No data for that year, please run get_data()')
-                return 'N/A'
-            self.df = self.df[['AGEP', 'SCH', 'SCHL']]
-            # calcualte the mean of years of schooling
-            self.df2 = self.df[self.df['AGEP'] > 25].copy()
-            self.df2['scholing'] =  self.df2['SCHL']
-            self.df2.reset_index(inplace=True)
-            self.df2['scholing'].replace({3:1, 4:2, 5:3, 6:4, 7:5, 8:6, 9:7, 10:8, 11:9, 
-                                    12:10, 13:11, 14:12, 15:13, 15:13, 16:13, 17:13, 
-                                    18:13.5, 19:14, 20:15, 21:17, 22:19, 23:19, 24:23}, inplace=True)
-            self.df2['enroled'] = np.where(self.df2['scholing'] > 1, 1, 0)
-            value1 = self.df2['scholing'].mean()
+    def edu_index(self, folder_path):
+        edu_index = pd.DataFrame(columns=['Year', 'edu_index'])
+        for file in os.listdir(folder_path):
+            if not file.endswith('.csv'):
+                continue
+            else:
+                self.df = pd.read_csv(os.path.join(folder_path, file))
+                self.df = self.df[['AGEP', 'SCH', 'SCHL']]
+                # calcualte the mean of years of schooling
+                self.df2 = self.df[self.df['AGEP'] > 25].copy()
+                self.df2['scholing'] =  self.df2['SCHL']
+                self.df2.reset_index(inplace=True)
+                self.df2['scholing'].replace({3:1, 4:2, 5:3, 6:4, 7:5, 8:6, 9:7, 10:8, 11:9, 
+                                        12:10, 13:11, 14:12, 15:13, 15:13, 16:13, 17:13, 
+                                        18:13.5, 19:14, 20:15, 21:17, 22:19, 23:19, 24:23}, inplace=True)
+                self.df2['enroled'] = np.where(self.df2['scholing'] > 1, 1, 0)
+                value1 = self.df2['scholing'].mean()
 
-            # calculate the expected years of schooling
-            self.df3 = self.df[self.df['AGEP'] < 25].copy()
-            self.df3['enrolled'] = self.df3['SCH'].apply(lambda x: 1 if x > 1 else 0)
-            self.df_age = self.df3.groupby(['AGEP'])[['AGEP','enrolled']].count()
-            self.df_age['enrolled'] = self.df3.groupby(['AGEP'])['enrolled'].sum()
-            self.df_age['enrollment_rate'] = self.df_age['enrolled'] / self.df_age['AGEP']
-            self.df_age = self.df_age.rename (columns = {'AGEP': 'count'})
-            self.df_age = self.df_age.reset_index()
-            self.df_age.drop([0,1,2,3,4], inplace=True)
-            value2 = self.df_age['enrollment_rate'].sum()
+                # calculate the expected years of schooling
+                self.df3 = self.df[self.df['AGEP'] < 25].copy()
+                self.df3['enrolled'] = self.df3['SCH'].apply(lambda x: 1 if x > 1 else 0)
+                self.df_age = self.df3.groupby(['AGEP'])[['AGEP','enrolled']].count()
+                self.df_age['enrolled'] = self.df3.groupby(['AGEP'])['enrolled'].sum()
+                self.df_age['enrollment_rate'] = self.df_age['enrolled'] / self.df_age['AGEP']
+                self.df_age = self.df_age.rename (columns = {'AGEP': 'count'})
+                self.df_age = self.df_age.reset_index()
+                self.df_age.drop([0,1,2,3,4], inplace=True)
+                value2 = self.df_age['enrollment_rate'].sum()
 
-            # calculate index
-            edu_index = (value1/15 + value2/18) / 2
-            return edu_index, value1, value2
+                # calculate index
+                edu_value = (value1/15 + value2/18) / 2
+                year = file.split('_')[1]
+                edu_index = edu_index.append({'Year': year, 'edu_index': edu_value}, ignore_index=True)
+
+        edu_index.to_csv('Data/edu_index.csv', index=False)
+
     
     def idh_index(self, year=2019, debug=False):
         # check if idh index is already calculated
