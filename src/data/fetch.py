410f2d6bc787084076e9d27911058ef44da31a90
Alejandro Ouslan <unclearcoder@hotmail.com>
2023-12-07 15:31:12 -0400

    changed download to use spinners

--- src/data/fetch.py
+++ src/data/fetch.py
@@ -3,30 +3,26 @@ import zipfile
 from tqdm import tqdm
 from src.data.IDH import IndexIDH
 import requests 
+from urllib.request import urlretrieve
+from yaspin import yaspin
 import os
 from rich import print
 from rich.console import Console
 
 console = Console()
 def get_data(range_years, data_file):
-    console.log("Downloading PPR data from the Census Bureau...", style='bold cyan')
+    
     for year in range(int(range_years[0]), int(range_years[1])+1):
+        spinner = yaspin(text='Downloading PPR data from the Census Bureau...', color='blue', spinner='dots')
         if os.path.exists(f'data/raw/data_{data_file[4:7]}_{year}_raw.csv') or os.path.exists(f'data/processed/edu_index.csv'):
             continue
         else:
             url = f'https://www2.census.gov/programs-surveys/acs/data/pums/{year}/5-Year/{data_file}'
             file_name = f'data/raw/raw_ppr_{year}.zip'
-
+            
             # Download progress bar
-            r = requests.get(url, stream=True)
-            total_size = int(r.headers.get('content-length'))
-            block_size = 1024
-            progress = tqdm(unit='iB', total=total_size, unit_scale=True, unit_divisor=1024, desc=f'Downloading {year}_{data_file[4:7]} data')
-            with open(file_name, 'wb') as f:
-                for data in r.iter_content(block_size):
-                    progress.update(len(data))
-                    f.write(data)
-            progress.close()
+            spinner = yaspin(text='Downloading PPR data from the Census Bureau...', color='blue', spinner='dots')
+            urlretrieve(url, file_name)
 
             # unzip the file
             with zipfile.ZipFile(file_name, 'r') as zip_ref:
@@ -42,19 +38,26 @@ def get_data(range_years, data_file):
                     os.rename(f'data/raw/{file}', f'data/raw/data_{data_file[4:7]}_{year}_raw.csv')
                 else:
                     continue
-        console.log(f'Done downloading {year} data', style='bold cyan')
 
        
 
 def calculate(range_years):
+    spinner = yaspin(text='Downloading PPR data from the Census Bureau...', color='blue', spinner='dots')
+    spinner.start()
     get_data(range_years, data_file='csv_ppr.zip')
+    spinner.stop()
+    spinner = yaspin(text='Downloading HPR data from the Census Bureau...', color='green', spinner='dots')
+    spinner.start()
     get_data(range_years, data_file='csv_hpr.zip')
-    console.log("Calculating all indexes...", style='bold cyan')
+    spinner.stop()
+    spinner = yaspin(text='Calculating IDH...', color='red', spinner='dots')
+    spinner.start()
     IDH = IndexIDH()
     IDH.edu_index('data/raw/')
     IDH.income_index()
     IDH.health_index()
     IDH.idh_index()
+    spinner.stop()
     
     # for file in os.listdir('data/raw/'):
     #     if file.endswith('raw.csv'):
