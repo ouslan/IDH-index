2ec1c10162e8e963c9338977185cc78b3a6b13cb
Alejandro Ouslan <unclearcoder@hotmail.com>
2023-12-14 15:04:08 -0400

    Meno beta 1.2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.data.IDH import IndexIDH
-from src.data.fetch import calculate
+from src.data.fetch import download
 import pandas as pd
 from time import sleep
 import numpy as np
@@ -9,12 +9,12 @@ from rich.console import Console
 from rich.table import Table
 
 def main():
-    options = ["Download IDH data", "Show Health Index", "Show Education Index", "Show Income Index", "Graph", "Exit"]
+    options = ["Download IDH data", "Show Health Index", "Show Education Index", "Show Income Index","IDH Index", "Graph", "Exit"]
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
     # downlad the data
     if menu_entry_index == 0:
-        calculate(['2012','2021'])
+        download(['2012','2021'])
     # show the health index
     elif menu_entry_index == 1:
         if os.path.isfile("data/processed/idh_index.csv"):
@@ -25,7 +25,9 @@ def main():
             table.add_column("Health Index")
             table.add_column("Health Index Ajusted")
             for i in range(len(df)):
-                table.add_row(np.round(str(df.iloc[i,0])), np.round(str(df.iloc[i,1])), np.round(str(df.iloc[i,2])))
+                table.add_row(str(np.round(df.iloc[i,0], decimals=2)),
+                              str(np.round(df.iloc[i,1],decimals=2)),
+                              str(np.round(df.iloc[i,2], decimals=2)))
             console = Console()
             console.print(table)
         else:
@@ -39,12 +41,45 @@ def main():
             table.add_column("Education Index")
             table.add_column("Education Index Ajusted")
             for i in range(len(df)):
-                table.add_row(str(np.round(df.iloc[i,0], decimals=2)), str(np.round(df.iloc[i,1],decimals=2)), str(np.round(df.iloc[i,2], decimals=2)))
+                table.add_row(str(np.round(df.iloc[i,0], decimals=2)), 
+                              str(np.round(df.iloc[i,1],decimals=2)), 
+                              str(np.round(df.iloc[i,2], decimals=2)))
             console = Console()
             console.print(table)
         else:
             print("No data found, please download the data first")
-    
+    elif menu_entry_index == 3:
+        if os.path.isfile("data/processed/idh_index.csv"):
+            df = pd.read_csv("data/processed/idh_index.csv")
+            df = df[['Year','income_index','income_index_ajusted']]
+            table = Table(show_header=True, title="Income Index")
+            table.add_column("Year")
+            table.add_column("Income Index")
+            table.add_column("Income Index Ajusted")
+            for i in range(len(df)):
+                table.add_row(str(np.round(df.iloc[i,0], decimals=2)), 
+                              str(np.round(df.iloc[i,1],decimals=2)), 
+                              str(np.round(df.iloc[i,2], decimals=2)))
+            console = Console()
+            console.print(table)
+        else:
+            print("No data found, please download the data first")
+    elif menu_entry_index == 4:
+        if os.path.isfile("data/processed/idh_index.csv"):
+            df = pd.read_csv("data/processed/idh_index.csv")
+            df = df[['Year','index','index_ajusted']]
+            table = Table(show_header=True, title="IDH Index")
+            table.add_column("Year")
+            table.add_column("IDH Index")
+            table.add_column("IDH Index Ajusted")
+            for i in range(len(df)):
+                table.add_row(str(np.round(df.iloc[i,0], decimals=2)), 
+                              str(np.round(df.iloc[i,1],decimals=2)),
+                              str(np.round(df.iloc[i,2], decimals=2)))
+            console = Console()
+            console.print(table)
+        else:
+            print("No data found, please download the data first")
 
 if __name__ == "__main__":
     main()
