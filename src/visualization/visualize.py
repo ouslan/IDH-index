18795312580e6a4637fa24e8c58bd933fcbc55e6
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-03-07 14:54:50 -0400

    added graph to the ui

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,38 +1,22 @@
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
 import pandas as pd
+import plotext as plt
 
 class Graphs:
 
-    def __init__(self, path, title):
-        self.path = pd.read_csv(path)
-        self.title = title
+    def __init__(self, path):
+        self.data = pd.read_csv(path)
 
-    def plot_graph(self):
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=self.path['Year'], y=self.path['index'], name=f'{self.title} Index', mode='lines+markers'))
-        # make an anotation for Huricane Maria
-        fig.add_annotation(x=2017, y=self.path.loc[self.path['Year'] == 2017].iat[0, 1], text=f'Huracan Maria', showarrow=True,
-                           font=dict(family="Courier New, monospace",
-                                     size=16,
-                                     color="#ffffff"),
-                           align="center", arrowhead=2, arrowsize=1, arrowwidth=2, arrowcolor="#636363", ax=20, ay=-30, 
-                           bordercolor="#c7c7c7", borderwidth=2, borderpad=4, bgcolor="#ff7f0e", opacity=0.8)
-        # make an anotation for Pandemic
-        fig.add_annotation(x=2020, y=self.path.loc[self.path['Year'] == 2020].iat[0, 1], text=f'La Pandemia', showarrow=True,
-                           font=dict(family="Courier New, monospace",
-                                     size=16,
-                                     color="#ffffff"),
-                           align="center", arrowhead=2, arrowsize=1, arrowwidth=2, arrowcolor="#636363", ax=-20, ay=30, 
-                           bordercolor="#c7c7c7", borderwidth=2, borderpad=4, bgcolor="#ff7f0e", opacity=0.8)
-        fig.update_layout(title=self.title)
-        fig.update_xaxes(title_text='Year')
-        fig.update_yaxes(title_text=f'{self.title} Index')
-        fig.update_layout(height=800, width=1200)
-        fig.show()
+    def plot_graph(self, y, x1, x2, x3, x4, title):
+        plt.plot(self.data[y], self.data[x1], color='red', label=x1)
+        plt.plot(self.data[y], self.data[x2], color='blue', label=x2)
+        plt.plot(self.data[y], self.data[x3], color='green', label=x3)
+        plt.plot(self.data[y], self.data[x4], color='black', label=x4)
+        plt.title(title)
+        plt.show()
+
+
+        
 
 if __name__ == '__main__':
-    path = 'Data/idh_index.csv'
-    title = 'Health'
-    g = Graphs(path, title)
-    g.plot_graph()
+    g = Graphs('/home/coder/Documents/Github/IDH-index/data/processed/idh_index.csv')
+    g.plot_graph('Year', 'index', 'health_index', 'income_index', 'edu_index', 'IDH Index')
