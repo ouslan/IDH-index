d02d4637319787094c07ea1a3716e7590123a8d0
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-08-05 10:44:00 -0400

    moved data retreival

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.data.data_pull import DataPull
 from src.data.data_process import DataProcess
 def main():
     DataPull(end_year=2022, debug=True)
-    DataProcess()
+    # DataProcess()
 
 
 if __name__ == "__main__":
