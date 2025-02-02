63e0a7e2fa879e8bdeb62d59b721e17b9a2c293f
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-02-02 13:38:52 -0400

    update the data from the wb to use ibis

--- src/models.py
+++ src/models.py
@@ -7,18 +7,35 @@ def get_conn(db_path: str) -> duckdb.DuckDBPyConnection:
 
 def init_pums_table(db_path: str) -> None:
     conn = get_conn(db_path)
-    conn.sql("DROP SEQUENCE IF EXISTS id_sequence;")
-    conn.sql("CREATE SEQUENCE id_sequence START 1;")
+    conn.sql("DROP SEQUENCE IF EXISTS pums_sequence;")
+    conn.sql("CREATE SEQUENCE pums_sequence START 1;")
     conn.sql(
         """
         CREATE TABLE IF NOT EXISTS "pumstable" (
-            id INTEGER PRIMARY KEY DEFAULT nextval('id_sequence'),
+            id INTEGER PRIMARY KEY DEFAULT nextval('pums_sequence'),
             year INTEGER,
             agep INTEGER,
             sch INTEGER,
             schl INTEGER,
             hincp INTEGER,
             pwgtp INTEGER
+        );
+        """
+    )
+
+
+def init_gni_table(db_path: str) -> None:
+    conn = get_conn(db_path=db_path)
+    conn.sql("DROP SEQUENCE IF EXISTS gni_sequence;")
+    conn.sql("CREATE SEQUENCE gni_sequence START 1;")
+    conn.sql(
+        """
+        CREATE TABLE IF NOT EXISTS "gnitable" (
+            id INTEGER PRIMARY KEY DEFAULT nextval('gni_sequence'),
+            year INTEGER,
+            constant FLOAT,
+            capita FLOAT,
+            life_exp FLOAT
         );
         """
     )
